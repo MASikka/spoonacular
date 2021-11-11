8fe31ea937c8b50c67c74b2a0b40bfedd0f64540
Marek <mareksikka@gmail.com>
2021-11-11 16:08:37 +0200

    Functional v1

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.get('/', (req,res)=>{
+    let url = `https://api.spoonacular.com/recipes/random/?apiKey=${process.env.APIKEY}`;
+
+    axios.get(url)
+    .then((response) =>{
+        let recipe = {
+            title: response.data.recipes[0].title,
+            summary: response.data.recipes[0].summary,
+            instructions: response.data.recipes[0].instructions,
+            imageUrl: response.data.recipes[0].image,
+            ingredients: response.data.recipes[0].extendedIngredients
+        }
+        res.render('index', {dataFromSpoonacular: recipe});
+    })
+    .catch(error=> {
+        console.log(error);
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}.`);
